6863912db3beb0d10ff4212e0add204eceb61f86
Shubham Gautam <138363357+ShuDeveloper@users.noreply.github.com>
2026-03-24 17:35:43 +0530

    Bootstrap  and add some files
    Home : show image of pdf first page
    index :  link pdf.js and bootstrap app
    pdfService : renderFirestPage is function return fist page image of pdf

---
+++ PDFManager/PDFManager/wwwroot/pdfService.js
@@ -0,0 +1,28 @@
+﻿//add pdf js worker
+pdfjsLib.GlobalWorkerOptions.workerSrc = "lib/pdf/dist/pdf.worker.js";
+
+//Get first page of pdf
+window.pdfService = {
+    renderFirstPage: async function (pdfUrl, canvasId) {
+        console.log("pdf service file work!");
+        const pdf = await pdfjsLib.getDocument(pdfUrl).promise;
+        const page = await pdf.getPage(1);
+
+        const scale = 1.5;
+        const viewport = page.getViewport({ scale: scale });
+
+        const canvas = document.getElementById(canvasId);
+        const context = canvas.getContext('2d');
+
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+
+        const renderContext = {
+            canvasContext: context,
+            viewport: viewport
+        };
+        await page.render(renderContext).promise;
+
+        return canvas.toDataURL("image/png"); // PNG data वापस देगा
+    }
+};
